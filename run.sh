395a9ae562ad15e3ef9e82a1a338ec56fa487683
Simone Messaggi <simone.messaggi@mia-platform.eu>
2020-03-02 11:42:55 +0100

    Testing this

--- run.sh
+++ run.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
 set -e
-echo '···································································································'
-echo 'MARTIN FABRIZZIO VILCHE - https://github.com/mvilche'
-echo '···································································································'
 
 if [ -z '$TIMEZONE' ]; then
 echo '···································································································'
-echo 'VARIABLE TIMEZONE NO SETEADA - INICIANDO CON VALORES POR DEFECTO'
-echo 'POSIBLES VALORES: America/Montevideo | America/El_Salvador'
+echo 'NO TIMEZONE SET - starting with Default values'
+echo 'Accepted values: America/Montevideo | America/El_Salvador'
 echo '···································································································'
 else
 echo '···································································································'
-echo 'TIMEZONE SETEADO ENCONTRADO: ' $TIMEZONE
+echo 'TIMEZONE ALREADY SET: ' $TIMEZONE
 echo '···································································································'
 cat /usr/share/zoneinfo/$TIMEZONE > /etc/localtime && \
 echo $TIMEZONE > /etc/timezone
 fi
-echo 'INICIANDO...'
+echo 'STARTING...'
 exec /usr/bin/go-tasks --allow-unprivileged --no-auto --verbose --include=/opt/crontabs --configfile=/opt/config/config.ini
